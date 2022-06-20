1bd88b072d04ce7fa9e5f9ad328c338ffd516fe6
designer (Designer) <no-reply@simplicite.fr>
2022-06-20 16:43:33 +0200

    Initial commit

---
+++ test/src/com/simplicite/tests/GitTest/GtsTest.java
@@ -0,0 +1,25 @@
+package com.simplicite.tests.GitTest;
+
+import java.util.*;
+
+import com.simplicite.util.*;
+import com.simplicite.util.exceptions.*;
+import com.simplicite.util.tools.*;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Unit tests
+ */
+public class GtsTest {
+	@Test
+	public void test() {
+		try {
+			AppLog.info("This is a unit test", null);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
